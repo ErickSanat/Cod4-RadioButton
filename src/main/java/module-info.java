7195f393913c6156a84e2a1e075e8123218a2c48
Erick R <erick12art@gmail.com>
2025-04-19 18:38:57 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cod4radiobutton {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cod4radiobutton to javafx.fxml;
+    exports com.example.cod4radiobutton;
+}
